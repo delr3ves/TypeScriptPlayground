84eca25792a90b53e3813811c5b69bb9537b33a6
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-10-22 08:45:30 +0200

    Create project with create-app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hi, this is the playground</h1>
+    </div>
+  );
+}
+
+export default App;
